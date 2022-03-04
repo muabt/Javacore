abf08870f2a829cced45849c6e5d65bb3404628c
muabt <hoamuacntt14@gmail.com>
2022-03-04 10:27:28 +0700

    Exercise day 3

--- src/main/java/org/example/day3/arrays/Exercise3.java
+++ src/main/java/org/example/day3/arrays/Exercise3.java
@@ -1,11 +1,16 @@
 package org.example.day3.arrays;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercise3 {
     public static void main( String[] args ) {
-       // maxMinArray();
+        maxMinArray();
         insertArray();
+        specialValue();
+        duplicateValue();
+        reverseArray();
+        findSecondLargest();
     }
 
        //Bai 3: Write a Java program to find the maximum and minimum value of an array.
@@ -71,5 +76,54 @@ public class Exercise3 {
 
         }
         //Bai 6: Write a Java program to test if an array contains a specific value.
+        private static boolean contain(int[] arr, int item){
 
-}
+            for (int n : arr) {
+                if (item == n) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private static void specialValue(){
+        int[] myArray = {20,30,40,50,60};
+        System.out.println(contain(myArray,40));
+
+    }
+        //Bai 7: Write a Java program to find the duplicate values of an array of string values
+        private static void duplicateValue() {
+            String[] myArray = {"Khoa hoc", "Nghe thuat", "Khoa hoc", "Kinh te", "Chinh tri"};
+            for (int i = 0; i < myArray.length - 1; i++) {
+                for (int j = i+1; j < myArray.length; j++) {
+                    if (myArray[i].equals(myArray[j]) && (i != j)) {
+                        System.out.println("Dupliate value array is:" + myArray[j]);
+                    }
+                }
+
+            }
+        }
+
+        //Bai 10: Write a Java program to reverse an array of integer values
+        private static void reverseArray(){
+        int[] myArray = {1, 2, 3, 4, 6, 7, 8, 9};
+        System.out.println("Mang goc:" + Arrays.toString(myArray));
+            for (int i = 0; i < myArray.length/2; i ++){
+                int temp = myArray[i];
+                myArray[i] = myArray[myArray.length - i -1];
+                myArray[myArray.length - i -1] = temp;
+            }
+            System.out.println("Mang dao chieu: " + Arrays.toString(myArray));
+        }
+        //Bai 11: Write a Java program to find the second largest element in an array.
+        private static void findSecondLargest(){
+            int[] myArray = {23, 45, 67, 78, 89, 105};
+            System.out.println("Mang goc:" + Arrays.toString(myArray));
+            Arrays.sort(myArray);
+            int index = myArray.length-1;
+            while (myArray[index] ==myArray[myArray.length-1]){
+                index --;
+
+            }
+            System.out.println("So lon thu hai cua mang: " +myArray[index]);
+        }
+    }
